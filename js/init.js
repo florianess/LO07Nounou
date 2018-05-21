5381f5617414358fcde31d8721818b712d1f9f01
florianess <florian.esslinger@live.fr>
2018-05-21 22:02:14 +0200

    Fix modals

--- js/init.js
+++ js/init.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var para = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(para);
-    var modalConnexion = document.getElementById(login);
-    var instances2 = M.Modal.init(modalConnexion);
-    var modalSignin = document.getElementById(signin);
-    var instances3 = M.Modal.init(modalSignin);    
+    var modal = document.querySelectorAll('.modal');
+    var instances2 = M.Modal.init(modal);
   });
