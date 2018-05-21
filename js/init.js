cd47280036fc2df65780c6f9364086a5c4d765b5
Florian Esslinger <florian.esslinger@live.fr>
2018-05-21 14:38:18 +0200

    Add todo and modal for inscription

--- js/init.js
+++ js/init.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var para = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(para);
-    var modal = document.querySelectorAll('.modal');
-    var instances2 = M.Modal.init(modal);
+    var modalConnexion = document.getElementById(login);
+    var instances2 = M.Modal.init(modalConnexion);
+    var modalSignin = document.getElementById(signin);
+    var instances3 = M.Modal.init(modalSignin);    
   });
