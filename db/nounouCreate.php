6cfaff5d11adad200bfd4162a8710ef255be5c35
Florian Esslinger <florian.esslinger@live.fr>
2018-05-23 12:55:41 +0200

    Add SQL file and change create nounou

--- db/nounouCreate.php
+++ db/nounouCreate.php
@@ -10,15 +10,19 @@ if ($test->num_rows == 0){
   $sql = "INSERT INTO utilisateur
   (nom, prenom, ville, email, portable,photo, age, experience, presentation, type_user, password)
   VALUES ('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['ville']."','".$_POST['email']."','".$_POST['portable']."','".$_FILES['photo']['tmp_name']."','".$_POST['age']."','".$_POST['experience']."','".$_POST['presentation']."','await','".password_hash($_POST["password"], PASSWORD_DEFAULT)."')";
-  $conn->query($sql);
-  $debutSql = "INSERT INTO utilisateur_has_langue (utilisateur_email, langue_id) VALUES ('" . $_POST['email'] . "','";
-  foreach ($_POST['langues'] as $value) {
-    $sql3 = $debutSql . $value . "')";
-    if (!$conn->query($sql3)) {
-      echo "Error: " . $sql3 . "<br>" . $conn->error . "<br>";
+  if ($conn->query($sql)) {
+    $debutSql = "INSERT INTO utilisateur_has_langue (utilisateur_email, langue_id) VALUES ('" . $_POST['email'] . "','";
+    foreach ($_POST['langues'] as $value) {
+      $sql3 = $debutSql . $value . "')";
+      if ($conn->query($sql3)) {
+        header('Location: ../index.html');
+      } else {
+        echo "Error: " . $sql3 . "<br>" . $conn->error . "<br>";
+      }
     }
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
   }
-  header('Location: ../index.html');
 } else {
   echo 'email deja utilisé';
 }
--- modules/candidats.php
+++ modules/candidats.php
@@ -16,31 +16,32 @@ if (isset($_GET['type'])) {
       break;
   }
   header('Location: ..\accueil\admin.php');
-}
+} ?>
 
-?>
 <div class='divider'></div>
 <div class='section'>
 <h3>Nouveau(x) Candidat(s)</h3>
 <p><?php echo $nounous->num_rows; ?> nounou(s) à valider </p>
-<table>
-<thead>
-  <tr>
-      <th>Nom</th>
-      <th>Prenom</th>
-      <th>Ville</th>
-      <th>Email</th>
-      <th>Portable</th>
-      <th>Age</th>
-      <th>Experience</th>
-      <th>Presentation</th>
-      <th>Valider</th>
-  </tr>
-</thead>
-<tbody>
-<?php
-if ($nounous->num_rows > 0){
-  while ($row = $nounous->fetch_row()) {
+
+<?php if ($nounous->num_rows > 0){ ?>
+
+  <table>
+  <thead>
+    <tr>
+        <th>Nom</th>
+        <th>Prenom</th>
+        <th>Ville</th>
+        <th>Email</th>
+        <th>Portable</th>
+        <th>Age</th>
+        <th>Experience</th>
+        <th>Presentation</th>
+        <th>Valider</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  <?php while ($row = $nounous->fetch_row()) {
     echo "<tr>";
       foreach ($row as $value) {
         echo "<td>".$value."</td>";
@@ -55,18 +56,4 @@ if ($nounous->num_rows > 0){
   echo "</tbody></table>";
 }
 echo "</div>";
-
-function add($value) {
-  global $conn;
-  $sql = "UPDATE utilisateur SET type_user='nounou' WHERE email='".$value."'";
-  $conn->query($sql);
-  header("Refresh:0");
-}
-
-function remove($value) {
-  global $conn;
-  $sql = "DELETE FROM utilisateur WHERE email='".$value."'";
-  $conn->query($sql);
-  header("Refresh:0");
-}
 ?>
