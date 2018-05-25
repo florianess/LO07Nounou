be1bfd23aaff0973174de994b94cb230f3043b66
Florian Esslinger <florian.esslinger@live.fr>
2018-05-25 16:18:41 +0200

    Fix enfant form and add test in nounou creation

--- Forms/EnfantsForm.php
+++ Forms/EnfantsForm.php
@@ -16,55 +16,6 @@
 
 require_once '../db/parentCreate.php';
 
-$formEnfants = <<<END
-
-<div class="row">
-  <div class="input-field col s6">
-    <input id="nom" type="text" class="validate" name ="nom[]">
-    <label for="nom">Nom</label>
-    </div>
-    </div>
-    <div class="row">
-
-    <div class="input-field col s6">
-      <input id="prenom" type="text" class="validate" name ="prenom[]">
-      <label for="prenom">Prénom</label>
-    </div>
-  </div>
-  <div class="row">
-    <div class="input-field col s6">
-      <input id="age" type="number" class="validate" name ="age[]">
-      <label for="age">Age</label>
-    </div>
-    </div>
-    <div class="row">
-      <div class="input-field col s1">
-        <label>
-        <input id="genreM" type="radio" class="validate" name ="genre[]" value="M">
-         <span>M</span>
-           </label>
-         </div>
-           <div class="input-field col s1">
-             <label>
-        <input id="genreF" type="radio" class="validate" name ="genre[]" value="F">
-         <span>F</span>
-           </label>
-
-      </div>
-      </div>
-      <br/>
-      </br>
-    <div class="row">
-      <div class="input-field col s6">
-        </br>
-      <textarea id="infos" name="info[]"  class="materialize-textarea">
-      </textarea>
-        <label for="infos">Informations particulières </label></br>
-      </div>
-    </div>
-    <hr/><br/>  </br>
-END;
-
 function formValide(){
   foreach ($_POST as $key => $value) {
     if ( $key!="filepath" &&  $key!="presentation") { //facultatif dans le formulaire
@@ -91,8 +42,7 @@ if(formValide() && $_POST["nbenfants"]>0){
   debutFormEnfants();
   for ($i=0; $i <$nombreEnfants ; $i++) {
     echo("<h3> Enfant ". ($i+1)."</h3>");
-    global $formEnfants;
-    echo $formEnfants;
+    formEnfants($i);
   }
 } else {
   header('Location: ParentsForm.html');
@@ -104,6 +54,55 @@ function debutFormEnfants(){
   </br>
   </br>
   <form class='col s12' method='post' action='../db/enfantCreate.php'>");
+}
+
+function formEnfants($i) {
+  print "
+  <div class='row'>
+    <div class='input-field col s6'>
+      <input id='nom$i' type='text' class='validate' name ='nom[]'>
+      <label for='nom$i'>Nom</label>
+      </div>
+      </div>
+      <div class='row'>
+
+      <div class='input-field col s6'>
+        <input id='prenom$i' type='text' class='validate' name ='prenom[]'>
+        <label for='prenom$i'>Prénom</label>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='input-field col s6'>
+        <input id='age$i' type='number' class='validate' name ='age[]'>
+        <label for='age$i'>Age</label>
+      </div>
+      </div>
+      <div class='row'>
+        <div class='input-field col s1'>
+          <label>
+          <input id='genreM$i' type='radio' class='validate' name ='genre[$i]' value='M'>
+           <span>M</span>
+             </label>
+           </div>
+             <div class='input-field col s1'>
+               <label>
+          <input id='genreF$i' type='radio' class='validate' name ='genre[$i]' value='F'>
+           <span>F</span>
+             </label>
+
+        </div>
+        </div>
+        <br/>
+        </br>
+      <div class='row'>
+        <div class='input-field col s6'>
+          </br>
+        <textarea id='infos' name='info[]'  class='materialize-textarea'>
+        </textarea>
+          <label for='infos'>Informations particulières </label></br>
+        </div>
+      </div>
+      <hr/><br/>  </br>";
 }
 
 ?>
--- db/nounouCreate.php
+++ db/nounouCreate.php
@@ -2,29 +2,59 @@
 
 require_once 'connection.php';
 
-$sql0 = "SELECT * FROM utilisateur WHERE email = '".$_POST["email"]."'";
+$regText = "/^([a-zA-Z]|\s)*$/";
 
-$test = $conn->query($sql0);
+$filters = array(
+  "nom" => array("filter"=>FILTER_VALIDATE_REGEXP,
+                 "options"=>array("regexp"=> $regText)),
+  "prenom" => array("filter"=>FILTER_VALIDATE_REGEXP,
+                    "options"=>array("regexp"=> $regText)),
+  "ville" => array("filter"=>FILTER_VALIDATE_REGEXP,
+                   "options"=>array("regexp"=> $regText)),
+  "email" => FILTER_VALIDATE_EMAIL,
+  "portable" => array("filter"=>FILTER_VALIDATE_REGEXP,
+                      "options"=>array("regexp"=>"/^0\d{9}$/")),
+  "age" => array("filter"=>FILTER_VALIDATE_INT,
+                "options"=>array("min_range"=>16,"max_range"=>60)),
+  "experience" => array("filter"=>FILTER_VALIDATE_REGEXP,
+                    "options"=>array("regexp"=> $regText)),
+  "presentation" => array("filter"=>FILTER_VALIDATE_REGEXP,
+                   "options"=>array("regexp"=> $regText)),
 
-if ($test->num_rows == 0){
-  $sql = "INSERT INTO utilisateur
-  (nom, prenom, ville, email, portable,photo, age, experience, presentation, type_user, password)
-  VALUES ('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['ville']."','".$_POST['email']."','".$_POST['portable']."','".$_FILES['photo']['tmp_name']."','".$_POST['age']."','".$_POST['experience']."','".$_POST['presentation']."','await','".password_hash($_POST["password"], PASSWORD_DEFAULT)."')";
-  if ($conn->query($sql)) {
-    $debutSql = "INSERT INTO utilisateur_has_langue (utilisateur_email, langue_id) VALUES ('" . $_POST['email'] . "','";
-    foreach ($_POST['langues'] as $value) {
-      $sql3 = $debutSql . $value . "')";
-      if ($conn->query($sql3)) {
-        $conn->close();
-        header('Location: ../?status=create');
-      } else {
-        echo "Error: " . $sql3 . "<br>" . $conn->error . "<br>";
+);
+
+$myinputs = filter_input_array(INPUT_POST, $filters);
+$values = array_values($myinputs);
+
+
+if(in_array(false, $values)) {
+  echo "ERREUR FORM";
+} else {
+  $sql0 = "SELECT * FROM utilisateur WHERE email = '".$_POST["email"]."'";
+
+  $test = $conn->query($sql0);
+
+  if ($test->num_rows == 0){
+    $sql = "INSERT INTO utilisateur
+    (nom, prenom, ville, email, portable,photo, age, experience, presentation, type_user, password)
+    VALUES ('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['ville']."','".$_POST['email']."','".$_POST['portable']."','".$_FILES['photo']['tmp_name']."','".$_POST['age']."','".$_POST['experience']."','".$_POST['presentation']."','await','".password_hash($_POST["password"], PASSWORD_DEFAULT)."')";
+    if ($conn->query($sql)) {
+      $debutSql = "INSERT INTO utilisateur_has_langue (utilisateur_email, langue_id) VALUES ('" . $_POST['email'] . "','";
+      foreach ($_POST['langues'] as $value) {
+        $sql3 = $debutSql . $value . "')";
+        if ($conn->query($sql3)) {
+          $conn->close();
+          header('Location: ../?status=create');
+        } else {
+          echo "Error: " . $sql3 . "<br>" . $conn->error . "<br>";
+        }
       }
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
     }
   } else {
-    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+    echo 'email deja utilisé';
   }
-} else {
-  echo 'email deja utilisé';
 }
+
 ?>
