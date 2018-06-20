198e06b4fb7405017e62ec4964f9c6f030202d07
charlenelecouturier <charlene.lecoutuier@gmail.com>
2018-06-20 11:57:59 +0200

    rien d'interessant, jessaie de faire le sticky footer

--- accueil/nounou.php
+++ accueil/nounou.php
@@ -40,6 +40,31 @@ if($res->num_rows == 0) {
     <br><br><hr><br>
     <div id='calendar'></div>
   </div>
+  <footer class="whpage-footer">
+            <div class="container">
+              <div class="row">
+                <div class="col l6 s12">
+                  <h5 class="black-text">Contactez nous</h5>
+                  <p class="grey-text text-darken-1">contact@NounouFinder.com</p>
+
+
+                </div>
+                <div class="col l4 offset-l2 s12">
+                  <h5 class="black-text">Services</h5>
+                  <ul>
+                    <li><a class="grey-text text-darken-1" href="../forms/NounouForm.html">Inscription en tant que Nounou</a></li>
+                    <li><a class="grey-text text-darken-1" href="../forms/ParentsForm.html">Inscription en tant que Parent</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class=" grey lighten-3 footer-copyright">
+              <div class="container">
+              © 2018 Copyright NounouFinder.com
+              <a class="grey-text text-darken-1 right" href="../index.php">Accueil</a>
+              </div>
+            </div>
+  </footer>
   <!--- Importer les librairies javacript nécessaires-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -50,30 +75,7 @@ if($res->num_rows == 0) {
   <script src="../js/initNounou.js"></script>
 
 </body>
-</br></br>
-<footer class="whpage-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="black-text">Contactez nous</h5>
-        <p class="grey-text text-darken-1">contact@NounouFinder.com</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="black-text">Services</h5>
-        <ul>
-          <li><a class="grey-text text-darken-1" href="forms/NounouForm.html">Inscription en tant que Nounou</a></li>
-          <li><a class="grey-text text-darken-1" href="forms/ParentsForm.html">Inscription en tant que Parent</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class=" grey lighten-3 footer-copyright">
-    <div class="container">
-    © 2018 Copyright nounoufinder.com
-    <a class="grey-text text-darken-1 right" href="../index.php">Accueil</a>
-    </div>
-  </div>
-</footer>
+
 <?php
 }
 ?>
--- accueil/parent.php
+++ accueil/parent.php
@@ -1,5 +1,4 @@
 <?php
-//Permet de vérifier si l'utilisateur est autorisé à accéder à la page
   session_start();
   if (isset($_SESSION['user']) && $_SESSION['user']['type_user'] == 'parent') {
 ?>
@@ -16,7 +15,7 @@
 
   <title>Compte Parent</title>
 </head>
-<body class="pink lighten-5">
+<body class="white">
   <nav class="white nav-extended">
     <div class="container nav-wrapper">
       <a id="logo-container" class="brand-logo grey-text text-darken-1" href="../">NounouFinder</a>
@@ -36,7 +35,6 @@
   </nav>
 </div>
 <?php
-//Affiche le module correspondant à chaque div
   echo '<div class="container" id="recherche">';
   require_once '../modules/recherche.php';
   echo '</div><div id="listegardes">';
@@ -47,35 +45,38 @@
 ?>
 </div>
 
-<footer class="white page-footer">
-  <div class="container white">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="black-text">Contactez nous</h5>
-        <p class="grey-text text-darken-1">contact@NounouFinder.com</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="black-text">Services</h5>
-        <ul>
-          <li><a class="grey-text text-darken-1" href="forms/NounouForm.html">Inscription en tant que Nounou</a></li>
-          <li><a class="grey-text text-darken-1" href="forms/ParentsForm.html">Inscription en tant que Parent</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class=" grey lighten-1 footer-copyright">
-    <div class="container">
-    © 2018 Copyright nounoufinder.com
-    <a class="white-text right" href="../index.html">Accueil</a>
-    </div>
-  </div>  </footer>
+<footer class="whpage-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="black-text">Contactez nous</h5>
+                <p class="grey-text text-darken-1">contact@NounouFinder.com</p>
+
+
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="black-text">Services</h5>
+                <ul>
+                  <li><a class="grey-text text-darken-1" href="../forms/NounouForm.html">Inscription en tant que Nounou</a></li>
+                  <li><a class="grey-text text-darken-1" href="../forms/ParentsForm.html">Inscription en tant que Parent</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class=" grey lighten-3 footer-copyright">
+            <div class="container">
+            © 2018 Copyright NounouFinder.com
+            <a class="grey-text text-darken-1 right" href="../index.php">Accueil</a>
+            </div>
+          </div>
+        </footer>
 
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
   <script type="text/javascript" src="../js/initParent.js"></script>
 </body>
 </html>
-</html>
+
 <?php } else {
   echo "Accèes refusé";
 } ?>
