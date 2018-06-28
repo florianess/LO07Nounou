908e62ea500df077209d9c601574dc4a8c4ef777
Florian Esslinger <florian.esslinger@live.fr>
2018-06-28 13:41:12 +0200

    fix eval

--- js/eval.js
+++ js/eval.js
@@ -3,6 +3,10 @@ function changeBib(numB) {
         var img = document.getElementById(`bib${i}`);
         img.src="../img/iconBiberon.png";
     }
+    for (let i = numB+1; i <= 5; i++) {
+        var img = document.getElementById(`bib${i}`);
+        img.src="../img/iconBiberonVide.png"
+    }
     var note = document.getElementById('note');
     note.value = numB.toString();
 }
